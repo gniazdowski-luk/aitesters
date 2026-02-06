6bab8c2a41d6f4397b6c91287f5a2adfd7faf166
Lukasz Gniazdowski <gniazdowski.luk@gmail.com>
2026-02-07 00:27:02 +0100

    trace on

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   reporter: [[ 'html' ], [ 'list' ]],
   use: {
     baseURL: 'http://localhost:3000',
-    trace: 'on-first-retry',
+    trace: 'on',
   },
   projects: [
     {
